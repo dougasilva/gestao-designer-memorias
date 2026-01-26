d47741f49331b7a0a7bc437175c6b492f90c13fd
Douglas Silva <douglasasilva@gmail.com>
2026-01-26 17:44:22 -0300

    API-01: setup inicial da Web API com EF Core, SQLite e DbContext

---
+++ GestaoDesignerMemorias.Api/Program.cs
@@ -0,0 +1,27 @@
+using GestaoDesignerMemorias.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite(
+        builder.Configuration.GetConnectionString("DefaultConnection"))
+);
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
