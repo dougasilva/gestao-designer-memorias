0455775d7289d1e8a0824916169143c4bec5bbaf
Douglas Silva <douglasasilva@gmail.com>
2026-01-27 20:23:54 -0300

    INT-01: webhook WhatsApp para captação automática de clientes

--- GestaoDesignerMemorias.Api/Program.cs
+++ GestaoDesignerMemorias.Api/Program.cs
@@ -13,6 +13,17 @@ builder.Services.AddDbContext<AppDbContext>(options =>
         builder.Configuration.GetConnectionString("DefaultConnection"))
 );
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("DevCors", policy =>
+    {
+        policy
+            .AllowAnyOrigin()
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
+
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
@@ -20,6 +31,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors("DevCors");
 
 app.UseHttpsRedirection();
 app.UseAuthorization();
