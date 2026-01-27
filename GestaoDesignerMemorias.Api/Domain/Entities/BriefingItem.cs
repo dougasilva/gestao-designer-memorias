cbde325a5ac06ca777231a105b22e2b34f0bd103
Douglas Silva <douglasasilva@gmail.com>
2026-01-27 09:15:30 -0300

    API-02: modelo de domínio com entidades e enums

---
+++ GestaoDesignerMemorias.Api/Domain/Entities/BriefingItem.cs
@@ -0,0 +1,14 @@
+﻿namespace GestaoDesignerMemorias.Domain.Entities
+{
+    public class BriefingItem
+    {
+        public Guid Id { get; set; }
+
+        public Guid PedidoId { get; set; }
+        public Pedido Pedido { get; set; } = null!;
+
+        public string Pergunta { get; set; } = null!;
+        public string? Resposta { get; set; }
+        public int Ordem { get; set; }
+    }
+}
---
+++ GestaoDesignerMemorias.Api/Domain/Enums/StatusPagamento.cs
@@ -0,0 +1,9 @@
+﻿namespace GestaoDesignerMemorias.Domain.Enums
+{
+    public enum StatusPagamento
+    {
+        Nenhum = 0,
+        SinalPago = 1,
+        Pago = 2
+    }
+}
