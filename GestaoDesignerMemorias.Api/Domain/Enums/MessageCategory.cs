e89214b31ff75cdcd5e25a97f65e97fb13fac3e0
Douglas Silva <douglasasilva@gmail.com>
2026-01-27 20:47:47 -0300

    INT-02: classificação de mensagens com switch no webhook; test: add unit tests for message classification

---
+++ GestaoDesignerMemorias.Api/Domain/Enums/MessageCategory.cs
@@ -0,0 +1,11 @@
+﻿namespace GestaoDesignerMemorias.Domain.Enums
+{
+    public enum MessageCategory
+    {
+        Orcamento,
+        Pedido,
+        Suporte,
+        Duvida,
+        Outros
+    }
+}
