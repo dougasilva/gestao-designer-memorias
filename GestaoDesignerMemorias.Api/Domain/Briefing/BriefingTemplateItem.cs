6537e2be98de22b1fee37c024d3b767032d561da
Douglas Silva <douglasasilva@gmail.com>
2026-01-28 17:46:54 -0300

    BRF-02: add briefing templates by tipo de evento + teste

---
+++ GestaoDesignerMemorias.Api/Domain/Briefing/BriefingTemplateItem.cs
@@ -0,0 +1,10 @@
+﻿using GestaoDesignerMemorias.Domain.Enums;
+
+namespace GestaoDesignerMemorias.Domain.Briefing;
+
+public class BriefingTemplateItem
+{
+    public string Pergunta { get; set; } = null!;
+    public BriefingItemType Tipo { get; set; }
+    public string? Opcoes { get; set; }
+}
